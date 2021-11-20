917f9f906d19dfc3a61fb1a3c6ac336fbca85b20
Kayla <kes353@cornell.edu>
2021-11-20 01:01:47 -0500

    added base swift files

---
+++ ios/CornellCookbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CornellCookbook
+//
+//  Created by Kayla Sprayberry on 11/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
